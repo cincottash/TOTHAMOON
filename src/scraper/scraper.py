6762df749d47e9adff3bfd3dd396e54c4cfb8738
cincottash <cincotta@udel.edu>
2021-01-28 19:27:20 -0500

    added ranking functionality

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,5 +1,41 @@
 from scraperSetup import *
 
 def scraperRun():
-	reddit, wsb = setup()
+	reddit, tickerScores, invalidChars = setup()
 	
+	#get top 1000 submissions
+	submissions = reddit.subreddit('wallstreetbets').top("week", limit=1000)
+
+	#loop through each submission title/body and keep track of all the DIFFERENT tickers found
+	for submission in submissions:
+		#keeps track of repeats so we only count tickers once per post
+		foundTickers = []
+		#loop through title
+		for word in submission.title.split(" "):
+			#normalize
+			word = word.lower()
+			for char in invalidChars:
+				word = word.replace(char, "")
+
+			#check if word is a ticker
+			if word in tickerScores and word not in foundTickers:
+				foundTickers.append(word)
+
+				#if a word is in the title it has more weight
+				tickerScores[word]+=2
+
+		#loop through body
+		for word in submission.selftext.split(" "):
+			#normalize
+			word = word.lower()
+			for char in invalidChars:
+				word = word.replace(char, "")
+
+			#check if word is a ticker
+			if word in tickerScores and word not in foundTickers:
+				foundTickers.append(word)
+
+				#if a word is in the body it has less weight
+				tickerScores[word]+=1
+
+	print(tickerScores)
--- src/scraper/scraperSetup.py
+++ src/scraper/scraperSetup.py
@@ -5,12 +5,24 @@ import sys
 
 def setup():
 
-	reddit = praw.Reddit(client_id='YgFnfjpWFbv66g', \
-	                     client_secret='jzxvfyzNhrle4G-G2QtttDqzsWM', \
-	                     user_agent='Subreddit Scraper', \
-	                     username='redeyesbigsmile', \
-	                     password='skrillex')
+	reddit = praw.Reddit(client_id='3dVFOGnTpVY0PA', \
+	                     client_secret='zjy3v8R1VzylnT0g8nYhJJFiThAcuA', \
+	                     user_agent='notabot', \
+	                     username='cincottashThrowAway', \
+	                     password='cincottashThrowAway')
 
-	wsb = reddit.subreddit('wallstreetbets')
 
-	return reddit, wsb
+	tickerScores = {
+		"gme":0,
+		"msft":0,
+		"bb":0,
+		"nok":0
+
+
+	}
+
+	#list of chars to filter out from words
+	invalidChars = ["$", ",", "."]
+
+
+	return reddit, tickerScores, invalidChars
