44568d144fd22af2d3324e9fb187dc4ae1913727
cincottash <cincotta@udel.edu>
2021-01-29 12:27:55 -0500

    added CLI

--- src/macd/setup.py
+++ src/macd/setup.py
@@ -1,14 +1,21 @@
 import sys
 def argParse():
 
-	try:
-		tracker  = sys.argv[1].upper()
+	# try:
+	# 	tracker  = sys.argv[1].upper()
 
-		shortPeriod = int(sys.argv[2])
+	# 	shortPeriod = int(sys.argv[2])
 
-		longPeriod = int(sys.argv[3])
-	except IndexError:
-		print("Error, correct usage is: python3 main.py tracker shortPeriod longPeriod")
-		exit(0)
+	# 	longPeriod = int(sys.argv[3])
+	# except IndexError:
+	# 	print("Error, correct usage is: python3 main.py tracker shortPeriod longPeriod")
+	# 	exit(0)
 
-	return tracker, shortPeriod, longPeriod
+	# return tracker, shortPeriod, longPeriod
+
+	ticker = input("Enter a ticker\n").upper()
+
+	shortPeriod = int(input("Enter the short EMA period\n"))
+	longPeriod = int(input("Enter the long EMA period\n"))
+
+	return ticker, shortPeriod, longPeriod
--- src/main.py
+++ src/main.py
@@ -5,12 +5,55 @@ from scraper import *
 sys.path.insert(1, 'macd/')
 from macd import *
 
+import time
+
+
+def getIndicator():
+	validInput = False
+
+	while(not validInput):
+
+		print("\nWhich indicator would you like to run?\n")
+		print("1) WSB Sentiment Analyzer")
+		print("2) MACD")
+		print("3) Exit")
+
+		try:
+			indicatorType = int(input())
+					
+			if(indicatorType > 0 and indicatorType < 4):
+				validInput = True
+			else:
+				print("Invalid input")
+				time.sleep(1)
+				
+		except ValueError:
+			print("Invalid input")
+			time.sleep(1)
+
+	return indicatorType
 
 def main():
+	print("If DFV can hold, so can you...")
+	end = False
 
-	#run each indicator for a given stock
-	#scraperRun()
-	macdRun()
+	while(not end):
+		
+		indicatorType = getIndicator()
+
+		if(indicatorType == 1):
+			print("Running WSB analyzer, this may take a while...\n")
+			scraperRun()
+		elif(indicatorType == 2):
+			print("Running MACD...\n")
+			macdRun()
+		elif(indicatorType == 3):
+			end=True
+	
+	print("Exiting program...")
+	time.sleep(1)
+	exit(0)
+
 
 
 if __name__ == '__main__':
