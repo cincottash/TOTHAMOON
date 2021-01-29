a82267f796a61ce5644ae7faa1e9c085f49fd1e7
cincottash <cincotta@udel.edu>
2021-01-29 10:28:53 -0500

    added macd

---
+++ src/macd/initialize.py
@@ -0,0 +1,60 @@
+import pyEX
+client = pyEX.Client(api_token='pk_93cbcaa185544c878c2ad10ad4ad226a' , version='stable', api_limit=2)
+
+
+def initializeStockData(tracker):
+
+	# chart(timeframe='1m', date=None, token='', version='', filter='')
+	#Historical price/volume data, daily and intraday NOT AS PANDAS DATEFRAME Remember no weekends!!!
+	stockData = client.chart(timeframe = "1y", symbol = tracker)
+
+	#Grab the newest days
+	stockData.reverse()
+
+	timeStamps = []
+	closePrices = []
+
+	for stock in stockData[len(stockData):]:
+		stockData.remove(stock)
+
+	#oldest stock at position 0
+	stockData.reverse()
+
+	
+
+	for stock in stockData:
+		#Trim "2020-"
+		#print(stock["date"])
+		timeStamps.append(stock["date"][::])
+		
+		closePrices.append(stock["open"])
+
+	return stockData, timeStamps, closePrices
+
+
+#Do EMA calculations with the initial data
+def initializeEMA(stockData, periodLen):
+	EMA = []
+
+	closePrices = []
+	for stock in stockData:
+		closePrices.append(stock["open"])
+
+	#the seed value is used to calculate the first value of each EMA, the seed value is an SMA of the period len in days
+	seedValue = 0
+	for i, price in enumerate(closePrices[:periodLen]):
+		if i > periodLen - 1:
+			break
+		seedValue += price
+
+	seedValue /= periodLen
+
+	for j, price in enumerate(closePrices[periodLen:]):
+		#Use seed value for the first ema calculation
+		if(j == 0):
+			newEMA = (price * (2/(periodLen + 1))) + (seedValue * (1 - (2/(periodLen + 1))))
+			EMA.append(newEMA)
+		else:
+			newEMA = (price * (2/(periodLen + 1))) + (EMA[j - 1] * (1 - (2/(periodLen + 1))))
+			EMA.append(newEMA)
+	return EMA
---
+++ src/macd/plot.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotData(timeStamps, closePrices, longEMA, shortEMA, longPeriod, shortPeriod, plotTitle):
+
+	signalLines = []
+
+	#Spin up period equal to the length of the period
+	count = 0
+	while(count < longPeriod):
+		longEMA.insert(0, None)
+		count += 1
+
+	count = 0
+	while(count < shortPeriod):
+		shortEMA.insert(0, None)
+		count += 1
+
+	timeStamps = np.array(timeStamps)
+	closePrices = np.array(closePrices)
+	shortEMA = np.array(shortEMA)
+	longEMA = np.array(longEMA)
+	plt.plot(timeStamps, closePrices)
+	plt.plot(timeStamps, shortEMA)
+	plt.plot(timeStamps, longEMA)
+
+	#Signal Lines, find when the long ema overtakes the short EMA and vice-versa
+	w = 0
+	while(w < len(timeStamps)):
+
+		placed = 0
+		if(w == 0):
+			signalLines.append(0)
+			placed = 1
+			pass
+		elif(shortEMA[w-1] != None and longEMA[w-1] != None):
+			#Long overtake short
+			if(shortEMA[w-1]-longEMA[w-1] >= 0 and shortEMA[w] - longEMA[w] <= 0):
+				signalLines.append(shortEMA[w])
+				placed = 1
+			#short overtakes long
+			elif(shortEMA[w-1] - longEMA[w-1] <= 0 and shortEMA[w] - longEMA[w] >= 0):
+				signalLines.append(shortEMA[w])
+				placed = 1
+		if(not placed):
+			signalLines.append(0)
+		w += 1
+
+	for i, signal in enumerate(signalLines):
+		if(signal != 0):
+			plt.axvline(x=timeStamps[i], linestyle='--')
+
+	plt.legend(["Open Prices", "Short EMA", "Long EMA", "Signal Lines"])
+
+	plt.xlabel("Date")
+	plt.xlim(0, len(timeStamps))
+	plt.title(plotTitle)
+
+	degrees = 270
+
+	plt.xticks(rotation=degrees)
+	
+	print("\nTotal data points: {}".format(len(timeStamps)))
+
+	plt.show()
